f27640aa682cf409fa923e93b79bd1ccd3570a2d
Wizardy <wizardhassan99@gmail.com>
2024-02-15 19:39:53 +0100

    commit

---
+++ src/controllers/usersController.ts
@@ -0,0 +1,13 @@
+import express  from "express";
+
+export const getAllUsers = async () => {
+    // get users code 
+};
+  
+export const deleteUser = async () => {
+    // delete user code
+}
+  
+export const updateUser = async () => {
+    // update user code
+}
