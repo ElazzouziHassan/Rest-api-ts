7dca1e71e7e371cdc0c985ada9e4a2cb22767bf4
Wizardy <wizardhassan99@gmail.com>
2024-02-15 19:22:40 +0100

    commit

--- src/database/users.ts
+++ src/database/users.ts
@@ -9,3 +9,14 @@ const UserSchema = new mongoose.Schema({
         sessionToken: { type: String, select: false},
     },
 });
+
+export const UserModel = mongoose.model('User', UserSchema);
+
+// actions on user :
+export const getUsers = () => UserModel.find();
+export const getUserByEmail = (email: String) => UserModel.findOne({email});
+export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne({ 'authentication.sessionToken': sessionToken });
+export const getUserById = (id: String) => UserModel.findById(id);
+export const createUser = (values: Record<string, any>) => new UserModel(values).save().then(user => user.toObject());
+export const deleteUserById = (id: String) => UserModel.findOneAndDelete({_id: id});
+export const updateUserById = (id: String, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values);
