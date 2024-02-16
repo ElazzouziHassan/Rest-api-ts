45a28e0f337a30d9b0debe81b561823d96fddaba
Wizardy <wizardhassan99@gmail.com>
2024-02-16 16:44:59 +0100

    commit

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -3,7 +3,7 @@ import express  from "express";
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
     try{
-        const users = await getUsers;
+        const users = await getUsers();
         return res.status(200).json(users);
     }catch (error){
         console.log(error);
