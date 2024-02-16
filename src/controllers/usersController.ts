1529e9a8ea609fa8cbadc546634e5f9c5018aba7
Wizardy <wizardhassan99@gmail.com>
2024-02-16 16:33:24 +0100

    get users

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,7 +1,14 @@
+import { getUsers } from "database/users";
 import express  from "express";
 
-export const getAllUsers = async () => {
-    // get users code 
+export const getAllUsers = async (req: express.Request, res: express.Response) => {
+    try{
+        const users = await getUsers;
+        return res.status(200).json(users);
+    }catch (error){
+        console.log(error);
+        return res.sendStatus(400);
+    }
 };
   
 export const deleteUser = async () => {
