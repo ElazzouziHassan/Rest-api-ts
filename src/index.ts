1e364c0725e92b1755cba178364ea22fa0053b02
wizardy <wizardhassan99@gmail.com>
2024-02-23 18:36:31 +0100

    commit

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,11 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import dotenv from 'dotenv'
 
 import mongoose from 'mongoose';
 
+dotenv.config()
 const app = express();
 
 app.use(cors({
@@ -25,7 +27,7 @@ server.listen(8080, () => {
     console.log('Server running on http://localhost:8080/')
 })
 
-const MONGO_URL = ''; // DB URL
+const MONGO_URL = 'mongodb+srv://'+process.env.DB_USERNAME+':'+process.env.DB_PASSWORD+'@cluster0.hmjah5l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'; // DB URL
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
